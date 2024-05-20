d71291e5d7e9c4c5bbc68396cfa1ceeaee26c0d2
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-05-20 09:52:27 +0300

    update README.md

--- src/main/kotlin/io/github/susmisek/springaisamples/SpringAiSamplesApplication.kt
+++ src/main/kotlin/io/github/susmisek/springaisamples/SpringAiSamplesApplication.kt
@@ -2,10 +2,12 @@ package io.github.susmisek.springaisamples
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import javax.annotation.processing.Generated
 
 @SpringBootApplication
 class SpringAiSamplesApplication
 
+@Generated
 fun main(args: Array<String>) {
     runApplication<SpringAiSamplesApplication>(*args)
 }
