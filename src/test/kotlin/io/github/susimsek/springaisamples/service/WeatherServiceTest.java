0da60713fc468b00aa92c08974fc7fc756b1b277
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-05-29 21:24:37 +0300

    native support

---
+++ src/test/kotlin/io/github/susimsek/springaisamples/service/WeatherServiceTest.java
@@ -0,0 +1,59 @@
+package io.github.susimsek.springaisamples.service;
+
+import io.github.susimsek.springaisamples.client.WeatherClient;
+import io.github.susimsek.springaisamples.model.Condition;
+import io.github.susimsek.springaisamples.model.Current;
+import io.github.susimsek.springaisamples.model.Location;
+import io.github.susimsek.springaisamples.model.WeatherRequest;
+import io.github.susimsek.springaisamples.model.WeatherResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class WeatherServiceTest {
+
+    @Mock
+    private WeatherClient weatherClient;
+
+    @InjectMocks
+    private WeatherService weatherService;
+
+    private WeatherRequest weatherRequest;
+    private WeatherResponse weatherResponse;
+
+    @BeforeEach
+    void setUp() {
+        weatherRequest = new WeatherRequest("Istanbul");
+
+        Location location = new Location(
+            "Istanbul", "Marmara", "Turkey", 41.01, 28.98, "Europe/Istanbul", 1625247600L, "2023-05-25 15:00");
+
+        Condition condition = new Condition("Sunny", "Sunny icon", 1000);
+
+        Current current = new Current(
+            1625247600L, LocalDateTime.now(), 20.0, 68.0, 1, condition,
+            10.0, 16.0, 180, "S", 1015.0, 30.0, 5.0, 0.2, 50, 75,
+            20.0, 68.0, 20.0, 68.0, 25.0, 77.0, 15.0, 59.0, 10.0, 6.2, 5.0, 12.0, 19.2);
+
+        weatherResponse = new WeatherResponse(location, current);
+    }
+
+    @Test
+    void testApply() {
+        when(weatherClient.getCurrentWeather(anyString())).thenReturn(weatherResponse);
+
+        WeatherResponse response = weatherService.apply(weatherRequest);
+
+        assertEquals(weatherResponse, response);
+    }
+}
