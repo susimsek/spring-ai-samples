9c86f3e826021410bcf69578cb17b24cd2be6d73
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-06-16 13:27:08 +0300

    jws example

--- src/main/kotlin/io/github/susimsek/springaisamples/config/SecurityConfig.java
+++ src/main/kotlin/io/github/susimsek/springaisamples/config/SecurityConfig.java
@@ -29,6 +29,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -193,8 +194,14 @@ public class SecurityConfig {
                 mvc.pattern("/*.webapp")).permitAll()
             .requestMatchers(mvc.pattern("/swagger-ui.html"), mvc.pattern("/swagger-ui/**"),
                 mvc.pattern("/v3/api-docs/**")).permitAll()
-            .requestMatchers(mvc.pattern("/api-docs/**")).permitAll()
+            .requestMatchers(
+                mvc.pattern(HttpMethod.GET, "/**"),
+                mvc.pattern(HttpMethod.HEAD, "/**"),
+                mvc.pattern(HttpMethod.OPTIONS, "/**"),
+                mvc.pattern(HttpMethod.TRACE, "/**")
+            ).permitAll()
             .requestMatchers(mvc.pattern("/actuator/**")).permitAll()
+            .requestMatchers(mvc.pattern("/api-docs/**")).permitAll()
             .requestMatchers(mvc.pattern("/api/security/sign")).permitAll()
             .anyRequest().signed()
             .build();
