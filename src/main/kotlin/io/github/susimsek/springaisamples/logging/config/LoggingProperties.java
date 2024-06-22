9560aa8f03391061410da7ce699af5c83a2ea1ce
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-06-22 22:43:28 +0300

    scp config

--- src/main/kotlin/io/github/susimsek/springaisamples/logging/config/LoggingProperties.java
+++ src/main/kotlin/io/github/susimsek/springaisamples/logging/config/LoggingProperties.java
@@ -1,8 +1,6 @@
 package io.github.susimsek.springaisamples.logging.config;
 
 import io.github.susimsek.springaisamples.logging.enums.LogLevel;
-import io.github.susimsek.springaisamples.logging.model.PathRule;
-import java.util.Collections;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
@@ -21,8 +19,6 @@ public class LoggingProperties {
         private boolean enabled = true;
         private LogLevel level = LogLevel.BASIC;
         private Obfuscate obfuscate = new Obfuscate();
-        private Exclude exclude = new Exclude();
-        private Include include = new Include();
     }
 
     @Getter
@@ -33,17 +29,5 @@ public class LoggingProperties {
         private List<String> headers;
         private List<String> parameters;
         private List<String> jsonBodyFields;
-    }
-
-    @Getter
-    @Setter
-    public static class Exclude {
-        private List<PathRule> rules = Collections.emptyList();
-    }
-
-    @Getter
-    @Setter
-    public static class Include {
-        private List<PathRule> rules = List.of(new PathRule("/**", null));
     }
 }
