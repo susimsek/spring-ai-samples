2c3695b3db781476c37e64e58fcf1754394f96e3
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-07-02 09:43:28 +0300

    scp config

--- src/main/kotlin/io/github/susimsek/springaisamples/config/AsyncConfig.java
+++ src/main/kotlin/io/github/susimsek/springaisamples/config/AsyncConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration(proxyBeanMethods = false)
 @EnableAsync
+@EnableScheduling
 @RequiredArgsConstructor
 public class AsyncConfig implements AsyncConfigurer {
 
